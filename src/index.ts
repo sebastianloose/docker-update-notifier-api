769a31d996bb590987b88e871caf5c625c2ce892
Sebastian Loose <sebastian.loose@yahoo.de>
2021-10-07 17:06:23 +0200

    Minor

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ initRepositoryWatcher();
 
 const app = express();
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/", router);
 
